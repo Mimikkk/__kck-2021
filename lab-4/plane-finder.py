aca80da8e91fb451e52d1d0b26c08c3e2643c73e
Hououin Kyouma <inwazzin@gmail.com>
2021-11-18 15:11:21 +0100

    17/21

--- lab-4/plane-finder.py
+++ lab-4/plane-finder.py
@@ -1,33 +1,27 @@
 import os
+
 import matplotlib.pyplot as plt
 from typing import *
 import numpy as np
+from scipy.ndimage import *
 from skimage import filters
 import skimage.color as skic
-from skimage.measure import find_contours
+from skimage.measure import *
 from skimage.morphology import *
+from skimage.util import *
 
 # region [Typing]
-
 Image = np.ndarray
 T = TypeVar('T')
-
 # endregion
 # region [Utils]
 def foreach(iterable: Iterable[T], fn: Callable[[T], None]):
   [*map(lambda x: fn(*x), iterable)]
 
-def flow(*fns: List[Callable[[T], T]]) -> Callable[[Iterable[T]], Iterable[T]]:
-  def wrapper(iterable: Iterable[T]):
-    for fn in fns: iterable = map(fn, iterable)
-    return iterable
-  return wrapper
-
 def dynamic(**kwargs) -> object:
   return type('dynamic', (), kwargs)
 # endregion
 # region [Filesystem]
-
 planes_directory: str = 'resources/images/planes/'
 output_directory: str = 'outputs/images/planes/'
 def image_from(imagename: str, extension: str = 'jpg') -> Image:
@@ -39,21 +33,23 @@ def save_to(path: str, image: Image, extension: str = 'jpg', **kwargs):
 def show(image: Image, **kwargs):
   plt.imshow(image, **kwargs)
   plt.waitforbuttonpress(0.01)
-
 # endregion
 # region [Data]
-
 planenames = [f'samolot{i:02}' for i in range(21)]
 images = list(map(image_from, planenames))
 planes = dict(zip(planenames, images))
-
 # endregion
 
 def as_gray(image: Image) -> Image:
   return 1 - skic.rgb2gray(image)
 
+def dilate(image: Image):
+  dilation_seed = np.copy(image)
+  dilation_seed[1:-1, 1:-1] = image.min()
+  return reconstruction(dilation_seed, mask=image, method='dilation')
+
 def perform_magic(image: Image):
-  def create_contoured_image(closed) -> plt.Figure:
+  def create_contoured_image(closed) -> Image:
     def remove_ticks(axes: plt.Axes):
       axes.set_xticks([])
       axes.set_yticks([])
@@ -61,39 +57,43 @@ def perform_magic(image: Image):
     data: Tuple[plt.Figure, plt.Axes] = plt.subplots()
     (figure, axes) = data
 
-    for contour in find_contours(closed > 0.2, threshold):
-      axes.plot(contour[:, 1], contour[:, 0], linewidth=2)
+    axes.imshow(image)
+    mask = remove_small_holes(closed > threshold, connectivity=2)
 
+    for props in regionprops(labels := label(mask), image):
+      contour = find_contours(labels == props.label, threshold)[0]
+      if len(contour) < 300: continue
+      axes.plot(*reversed(contour.T), linewidth=2)
+      axes.plot(*reversed(props.centroid), 'wo', markersize=8)
     remove_ticks(axes)
+
     figure.savefig('temp.png')
     figure.clear()
-
     closed = plt.imread('temp.png')
     os.remove('temp.png')
     return closed[:, :, :3]
 
-  grayscale = as_gray(image)
-  threshold = filters.threshold_otsu(grayscale)
-  # mask = grayscale > threshold
-  grayscale = filters.median(grayscale, disk(5))
+  grayscale = filters.median(as_gray(image), disk(6))
+  dilation = dilate(grayscale)
+  dilated = grayscale - dilation
+  closed = closing(area_closing(1 - np.power(1 - dilated, 3), 2000), disk(14))
 
-  dilation_seed = np.copy(grayscale)
-  dilation_seed[1:-1, 1:-1] = grayscale.min()
-  dilated = reconstruction(dilation_seed, mask=grayscale, method='dilation')
-  closed = closing(grayscale - dilated, disk(8))
+  threshold = filters.threshold_otsu(closed)
+  contoured = create_contoured_image(closed)
 
   return dynamic(
     original=image,
-    gray=grayscale,
-    dilate=grayscale - dilated,
+    grayscale=grayscale,
+    dilation=dilation,
     dilated=dilated,
     closed=closed,
-    contoured=create_contoured_image(np.copy(closed)),
+    threshold=threshold,
+    contoured=contoured
   )
 
 def pm(nr): return perform_magic(planes[f"samolot{nr:02}"])
-# processed_planes = list(flow(as_gray, create_contoured_image)(images))
-# foreach(zip(planenames, processed_planes), save_to)
 
+processed_planes = list(map(lambda x: x.contoured, map(perform_magic, images)))
+foreach(zip(planenames, processed_planes), save_to)
 
-# mend mask with dilation
+save_to('collage', montage(processed_planes, multichannel=True))
